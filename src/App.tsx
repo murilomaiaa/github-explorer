056da2547cacc1dc75f4a5f0bf8e6c1fbe8b380b
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-04 14:01:41 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FunctionComponent = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
