cfbed06fd520c33e4de08ae01463f9c4b92edb1a
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-10 20:37:51 -0300

    Listing  statics infos about each repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi'
 
-const Repository: React.FC = () => (
-  <h1>
-    Repository
-  </h1>
-);
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles'
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (<>
+    <Header>
+      <img src={logo} alt="GitHub Explorer" />
+      <Link to="/">
+        <FiChevronsLeft size={16} />
+        Voltar
+      </Link>
+    </Header>
+    <RepositoryInfo>
+      <header>
+        <img src="https://avatars0.githubusercontent.com/u/56596799?v=4" alt="Murilo Maia" />
+        <div>
+          <strong>mrlmaia/pwfinal</strong>
+          <p>repository description</p>
+        </div>
+      </header>
+      <ul>
+        <li>
+          <strong>1000</strong>
+          <span>stars</span>
+        </li>
+        <li>
+          <strong>70</strong>
+          <span>forks</span>
+        </li>
+        <li>
+          <strong>81</strong>
+          <span>issues abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
+
+    <Issues>
+      <Link
+        to={`#`}
+      >
+        <div>
+          <strong>Issue</strong>
+          <p>Texto da issue</p>
+        </div>
+        <FiChevronsRight size={20} />
+      </Link>
+    </Issues>
+  </>)
+}
 
 export default Repository;
